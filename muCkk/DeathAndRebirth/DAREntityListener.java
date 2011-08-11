248e86ddcccca8b8cf8fb72234116fd604ba5041
muCkk <jb@.(none)>
2011-08-11 20:36:48 +0200

    pvp restrictions

--- muCkk/DeathAndRebirth/DAREntityListener.java
+++ muCkk/DeathAndRebirth/DAREntityListener.java
@@ -3,6 +3,7 @@ package muCkk.DeathAndRebirth;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityListener;
@@ -19,7 +20,7 @@ public class DAREntityListener extends EntityListener {
 	/**
 	 * Checks for dying players
 	 */
-	public void onEntityDeath(EntityDeathEvent event) {
+	public void onEntityDeath(EntityDeathEvent event) {		
 		Entity entity = event.getEntity();		
 		if(entity.getClass() == CraftPlayer.class) {
 			Player player = (Player) entity;
@@ -34,6 +35,7 @@ public class DAREntityListener extends EntityListener {
 		if (event.isCancelled()) {
 			return;
 		}
+		
 		try {
 			Entity target = event.getTarget();
 			if(target.getClass().equals(CraftPlayer.class)) {
@@ -51,10 +53,35 @@ public class DAREntityListener extends EntityListener {
 		if(event.isCancelled()) {
 			return;
 		}
+		
+		// *** a ghost tries to attack ***
+		if (event instanceof EntityDamageByEntityEvent) {
+			 Entity damager = ((EntityDamageByEntityEvent)event).getDamager();
+			 if (damager instanceof Player) {
+				 Player attacker = (Player) damager;
+				 if(ghosts.isGhost(attacker)) {
+					 DARMessages.cantDoThat(attacker);
+					 event.setCancelled(true);
+					 return;
+				 }
+			 }
+		}
+		
+		// *** a ghost is attacked ***
 		Entity entity = event.getEntity();
 		if(entity.getClass().equals(CraftPlayer.class)) {
 			Player player = (Player) entity;
 			if(ghosts.isGhost(player)) {
+				
+				// *** check if a player tries to attack a ghost ***
+				if (event instanceof EntityDamageByEntityEvent) {
+					 Entity damager = ((EntityDamageByEntityEvent)event).getDamager();
+					 if (damager instanceof Player) {
+						 Player attacker = (Player) damager;
+						 DARMessages.cantAttackGhosts(attacker);
+					 }
+				}
+				// *************************************************
 				event.setCancelled(true);
 			}
 		}
