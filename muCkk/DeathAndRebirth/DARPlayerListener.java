16170e52e1778c32b3492676a0ac430d8b82fed3
muCkk <jb@.(none)>
2011-08-11 20:37:32 +0200

    less code, most of it is now in the blocklistener

--- muCkk/DeathAndRebirth/DARPlayerListener.java
+++ muCkk/DeathAndRebirth/DARPlayerListener.java
@@ -1,25 +1,22 @@
 package muCkk.DeathAndRebirth;
 
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event.Result;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.Material;
 
 public class DARPlayerListener extends PlayerListener {
 
 	private DARHandler ghosts;
-	private DARGraves graves;
 	private DARShrines shrines;
 	
-	public DARPlayerListener(DARHandler ghosts, DARGraves graves, DARShrines shrines) {
+	public DARPlayerListener(DARHandler ghosts, DARShrines shrines) {
 		this.ghosts = ghosts;
-		this.graves = graves;
 		this.shrines = shrines;
 	}
 	/**
@@ -54,38 +51,33 @@ public class DARPlayerListener extends PlayerListener {
 	}
 	
 	/**
-	 * Resurrects a player when a shrine is clicked. 
-	 * Prevents dead players from interacting with chests and furnaces.
-	 * Protects graves.
+	 * Players try to bind their soul to a shrine
 	 */
 	public void onPlayerInteract(PlayerInteractEvent event) {
-		if(event.isCancelled()) {
-			return;
-		}
 		Player player = event.getPlayer();
-		Material type = event.getClickedBlock().getType();
-		if(type.equals(Material.FURNACE) || type.equals(Material.CHEST)) {
-			if(ghosts.isGhost(player)) {
-				DARMessages.cantUseThat(player);
-				event.setUseInteractedBlock(Result.DENY);
+		
+		// *** Ghosts can't interact with everything ***
+		if(ghosts.isGhost(player)) {
+			Material type = event.getClickedBlock().getType(); 
+			if(			!type.equals(Material.WOOD_DOOR)
+					||	!type.equals(Material.WOODEN_DOOR)
+					||	!type.equals(Material.STONE_BUTTON)
+					||	!type.equals(Material.LEVER)) {
+				
+				DARMessages.cantDoThat(player);
 				event.setCancelled(true);
+				return;
 			}
 		}
-		if(type.equals(Material.SIGN_POST)) {
-			Block block = event.getClickedBlock();
-			if (graves.isProtected(player.getName(), block.getX(), block.getY(), block.getZ())) {
-				DARMessages.graveProtected(player);
-				event.setUseInteractedBlock(Result.DENY);
-				event.setCancelled(true);
-			}
-		}
-		if(event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+		
+		// *** shrine is clicked ***
+		if(event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 			String shrine = shrines.getClose(player);
 			if (shrine != null) {
-				if(shrines.isShrine(shrine, event.getClickedBlock(), player)) {
-					DARMessages.shrineCantBeDestroyed(player);
-					event.setUseInteractedBlock(Result.DENY);
-					event.setCancelled(true);
+				Block clickedBlock = event.getClickedBlock();
+				if(shrines.isShrine(shrine, clickedBlock, player)) {
+					ghosts.bindSoul(player);
+					DARMessages.boundShrine(player);
 				}
 			}
 		}
