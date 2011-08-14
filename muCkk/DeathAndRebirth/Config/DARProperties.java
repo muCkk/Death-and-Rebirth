a6dfe4938acd309d41a2627c990e527f26ae26a8
muCkk <jb@.(none)>
2011-08-14 19:56:35 +0200

    added new configuration options (fly, shrineOnly, ghostChat, blockGhostInteraction)

--- muCkk/DeathAndRebirth/Config/DARProperties.java
+++ muCkk/DeathAndRebirth/Config/DARProperties.java
@@ -14,17 +14,25 @@ public class DARProperties extends Properties {
 	private String dir, fileName;
 	FileWriter writer;
 	FileReader reader;
-	private String info = "needItem: (true/false) Defines if an item is needed to resurrect players\n" 
-			+ "itemID: ID of the item which will be consumed\n"
-			+ "amount: Amount needed of that item to reserruct\n"
-			+ "distance: Maximal distance to the dead player"
-			+ "worldName: (true/false) Switch Death and Rebirth on and off for each world";
+	private String info = "needItem: (true/false) Defines if an item is needed to resurrect players.\n" 
+			+ "itemID: ID of the item which will be consumed.\n"
+			+ "amount: Amount needed of that item to reserruct.\n"
+			+ "distance: Maximal distance to the dead player.\n"
+			+ "worldName: (true/false) Switch Death and Rebirth on and off for each world.\n"
+			+ "blockGhostInteraction: (true/false) If set to true ghosts can't use doors, buttons and levers.\n"
+			+ "fly: (true/false) Defines if ghosts can fly.\n"
+			+ "shrineOnly: (true/false) If set to true dead players have to walk to a shrine. Deactivates soul binding.\n"
+			+ "ghostChat: (true/false) Enables or disables if ghosts can chat.";
 
 	private int distance = 10,
 			amount = 1,
 			itemID = 288;
 	
-	private boolean needItem = false;
+	private boolean needItem = false,
+					ghostInteraction = false,
+					fly = false,
+					shrineOnly = false,
+					ghostChat = true;
 			
 	public DARProperties(String dir, String fileName) {
 		this.dir = dir;
@@ -43,12 +51,16 @@ public class DARProperties extends Properties {
 			File directory = new File(dir);
 			if (!directory.exists())
 				directory.mkdir();
-			getBoolean("needItem", needItem);
-			getInteger("amount", amount);
-			getInteger("itemID", itemID);
-			getInteger("distance", distance);
-			save();
 		}
+		getBoolean("needItem", needItem);
+		getInteger("amount", amount);
+		getInteger("itemID", itemID);
+		getInteger("distance", distance);
+		getBoolean("blockGhostInteraction", ghostInteraction);
+		getBoolean("fly", fly);
+		getBoolean("shrineOnly", shrineOnly);
+		getBoolean("ghostChat", ghostChat);
+		save();
 	}
 
 	public void save() {
@@ -100,6 +112,26 @@ public class DARProperties extends Properties {
 	public boolean isSpoutEnabled() {
 		return getBoolean("spout");
 	}
+	
+	public boolean isCitizensEnabled() {
+		return getBoolean("citizens");
+	}
+	
+	public boolean isBlockGhostInteractionEnabled() {
+		return getBoolean("blockGhostInteraction");
+	}
+	
+	public boolean isFlyingEnabled() {
+		return getBoolean("fly");
+	}
+	
+	public boolean isShrineOnlyEnabled() {
+		return getBoolean("shrineOnly");
+	}
+	
+	public boolean isGhostChatEnabled() {
+		return getBoolean("ghostChat");
+	}
 	// *** setter  ***********************************************************
 	
 	public void setBoolean(String name, boolean bool) {
@@ -117,6 +149,50 @@ public class DARProperties extends Properties {
 		}
 		else {
 			put("spout", "false");
+		}
+	}
+
+	public void setCitizens(boolean b) {
+		if (b) {
+			put("citizens", "true");
+		}
+		else {
+			put("citizens", "false");
+		}
+	}
+	
+	public void setFly(boolean b) {
+		if (b) {
+			put("fly", "true");
+		}
+		else {
+			put("fly", "false");
+		}
+	}
+	
+	public void setShrineOnly(boolean b) {
+		if (b) {
+			put("shrineOnly", "true");
+		}
+		else {
+			put("shrineOnly", "false");
+		}
+	}
+	
+	public void setBlockGhostInteraction(boolean b) {
+		if (b) {
+			put("blockGhostInteraction", "true");
+		}
+		else {
+			put("blockGhostInteraction", "false");
+		}
+	}
+	public void setGhostChat(boolean b) {
+		if (b) {
+			put("ghostChat", "true");
+		}
+		else {
+			put("ghostChat", "false");
 		}
 	}
 }
