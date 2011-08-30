a6a85217c9e63500985f3ebb1ef66a567f6e9b8c
muCkk <themuckk@web.de>
2011-08-30 23:26:27 +0200

    grave signs are now replaced by the original block (saving half steps)

--- muCkk/DeathAndRebirth/ghost/DARGraves.java
+++ muCkk/DeathAndRebirth/ghost/DARGraves.java
@@ -3,8 +3,12 @@ package muCkk.DeathAndRebirth.ghost;
 import java.io.File;
 import java.util.List;
 
+import muCkk.DeathAndRebirth.config.DARProperties;
 import muCkk.DeathAndRebirth.messages.DARErrors;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.util.config.Configuration;
 
 public class DARGraves {
@@ -12,10 +16,12 @@ public class DARGraves {
 	private String dir;
 	private File graveFile;
 	private Configuration yml;
+	private DARProperties config;
 	
-	public DARGraves(String dir, String fileName) {
+	public DARGraves(String dir, String fileName, DARProperties config) {
 		this.dir = dir;
 		graveFile = new File(fileName);
+		this.config = config;
 	}
 	
 	public void load() {
@@ -43,20 +49,58 @@ public class DARGraves {
 		yml.save();
 	}
 	
-	public void addGrave(String name, int x, int y, int z, String l1, String l2, String world) {
-		yml.setProperty("graves." +world +"." +name+".x", x);
-		yml.setProperty("graves." +world +"." +name+".y", y);
-		yml.setProperty("graves." +world +"." +name+".z", z);
+//	public void addGrave(String name, int x, int y, int z, String l1, String l2, String world) {
+//		
+//		yml.setProperty("graves." +world +"." +name+".x", x);
+//		yml.setProperty("graves." +world +"." +name+".y", y);
+//		yml.setProperty("graves." +world +"." +name+".z", z);
+//		yml.setProperty("graves." +world +"." +name+".l1", l1);
+//		yml.setProperty("graves." +world +"." +name+".l2", l2);
+//		
+//		yml.save();
+//	}
+	public void addGrave(String name, Block block, String l1, String world) {
+		if (config.isSignsEnabled()) placeSign(block, l1, name);
+		
+		yml.setProperty("graves." +world +"." +name+".x", block.getX());
+		yml.setProperty("graves." +world +"." +name+".y", block.getY());
+		yml.setProperty("graves." +world +"." +name+".z", block.getZ());
 		yml.setProperty("graves." +world +"." +name+".l1", l1);
-		yml.setProperty("graves." +world +"." +name+".l2", l2);
+		yml.setProperty("graves." +world +"." +name+".l2", name);
 		
 		yml.save();
 	}
 	
-	public void deleteGrave(String name, String world) {
-		yml.removeProperty("graves." +world +"." +name);
-		yml.save();
+	public void deleteGrave(Block block, String name, String worldName) {
+		if (config.isSignsEnabled()) removeSign(block, name, worldName);
+		yml.removeProperty("graves." +worldName +"." +name);
+		yml.save();		
 	}
+	
+	private void placeSign(Block block, String l1, String name) {
+		int id = block.getTypeId();
+		yml.setProperty("graves."+block.getWorld().getName()+"."+name+".blockid", id);
+		if (id == 43 || id == 44 || id == 35) {
+			int data = (int) block.getData();
+			yml.setProperty("graves."+block.getWorld().getName()+"."+name+".blockdata", data);
+		}
+		block.setType(Material.SIGN_POST);
+		Sign sign = (Sign) block.getState();
+		sign.setLine(1, l1);
+		sign.setLine(2, name);
+		sign.update(true);
+	}
+	
+	private void removeSign(Block block, String name, String worldName) {
+		int id = yml.getInt("graves."+worldName+"."+name+".blockid", 0);
+		block.setTypeId(id);
+		if (id == 43 || id == 44 || id == 35) block.setData((byte)yml.getInt("graves."+worldName+"."+name+".blockdata", 0));
+	}
+	
+//	public void deleteGrave(String name, String world) {
+//		yml.removeProperty("graves." +world +"." +name);
+//		yml.save();
+//	}
 	
 	public boolean isProtected(String name, String world, int x, int y, int z) {
 		List<String> graves = yml.getKeys("graves." +world);
